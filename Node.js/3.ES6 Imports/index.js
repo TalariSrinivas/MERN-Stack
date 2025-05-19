f04b291d30414e5f0e6e0f2b953869ce870995c0
TalariSrinivas <165267278+TalariSrinivas@users.noreply.github.com>
2025-05-19 23:07:49 +0530

    Module Wrapper,Modules,ES6 Imports

---
+++ Node.js/3.ES6 Imports/index.js
@@ -0,0 +1,19 @@
+import greet from "./greet.js"
+//.js is necessary
+
+greet("Srinivas");
+
+//use npm init 
+//then go to package.json and add the below code
+/*{
+  "name": "3.es6-imports",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "description": ""
+}*/
---
+++ Node.js/index.js
@@ -0,0 +1,8 @@
+let money=true;
+
+if(money){
+    console.log("Buy");
+}
+else{
+    console.log("Can't Buy");
+}
